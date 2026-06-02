414547677b8a0adc3d22923c0bbdcf968f132495
Muhammad Ali <malishoaib.2001@gmail.com>
2026-06-03 00:42:03 +0500

    feat: update App component with BrowserRouter and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import theme from "./utils/theme";
+import AppBar from "./components/AppBar";
 
 const themeMode = createTheme(theme());
 
@@ -9,7 +11,16 @@ function App() {
   return (
     <>
       <ThemeProvider theme={themeMode}>
-        <Home />
+        <BrowserRouter>
+          <AppBar />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </main>
+        </BrowserRouter>
       </ThemeProvider>
     </>
   );
